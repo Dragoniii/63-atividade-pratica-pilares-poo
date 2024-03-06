3564a45a61390a277b91118739328ace2ace9909
Joel Luis Steffen <jo.lsteffen@gmail.com>
2024-03-06 11:20:57 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import {Animal, Cachorro, Cavalo, Gato} from './atividades/03'
+
+const meuAnimal = new Animal ('Dragonii', 15, 5)
+const meuCachorro = new Cachorro ('Dragonii', 15, 5)
+const meuCavalo = new Cavalo ('Dragonii', 15, 5)
+const meuGato = new Gato ('Dragonii', 15, 5)
+
+
+meuAnimal.comer()
+meuCachorro.latir()
+meuCavalo.galopar()
+meuGato.miar()
